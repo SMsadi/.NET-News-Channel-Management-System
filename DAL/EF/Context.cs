484449f47ec301f17c8ac4ab2a59f2c819e27538
S. M. SADI <smsadi2001@gmail.com>
2025-02-04 09:27:02 +0600

    Add files via upload

---
+++ DAL/EF/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class Context : DbContext
+    {
+        
+        public Context() : base("name=NewsAPI")
+        {
+        }
+
+        public DbSet<News> News { get; set; }
+    }
+}
---
+++ DAL/Repos/NewsRepo.cs
@@ -0,0 +1,86 @@
+﻿using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    public class NewsRepo
+    {
+        private readonly Context db;
+
+        public NewsRepo()
+        {
+            db = new Context();
+        }
+
+        public void Create(News news)
+        {
+            db.News.Add(news);
+            db.SaveChanges();
+        }
+
+        public List<News> Get()
+        {
+            return db.News.ToList();
+        }
+
+        public void Update(News news)
+        {
+            var exobj = Get(news.Id);
+            if (exobj != null)
+            {
+                db.Entry(exobj).CurrentValues.SetValues(news);
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var exobj = db.News.Find(id);
+            if (exobj != null)
+            {
+                db.News.Remove(exobj);
+                db.SaveChanges();
+            }
+        }
+
+        public News Get(int id)
+        {
+            return db.News.Find(id);
+        }
+
+        public List<News> GetByCategory(string category)
+        {
+            return db.News.Where(n => n.Category == category).ToList();
+        }
+
+        public List<News> GetByTitle(string title)
+        {
+            return db.News.Where(n => n.Title.Contains(title)).ToList();
+        }
+
+        public List<News> GetByDate(DateTime date)
+        {
+            return db.News.Where(n => DbFunctions.TruncateTime(n.Date) == DbFunctions.TruncateTime(date)).ToList();
+        }
+
+        public List<News> GetByTitleAndCategory(string title, string category)
+        {
+            return db.News.Where(n => n.Title.Contains(title) && n.Category == category).ToList();
+        }
+
+        public List<News> GetByTitleAndDate(string title, DateTime date)
+        {
+            return db.News.Where(n => n.Title.Contains(title) && DbFunctions.TruncateTime(n.Date) == DbFunctions.TruncateTime(date)).ToList();
+        }
+
+        public List<News> GetByTitleDateAndCategory(string title, DateTime date, string category)
+        {
+            return db.News.Where(n => n.Title.Contains(title) && DbFunctions.TruncateTime(n.Date) == DbFunctions.TruncateTime(date) && n.Category == category).ToList();
+        }
+    }
+}
